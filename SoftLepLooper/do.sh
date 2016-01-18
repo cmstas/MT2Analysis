de192f87f9c090bd14ee7c2d8eb055dc02154635
mderdzinski <mark.derdzinski@gmail.com>
2016-01-18 05:13:07 -0800

    update do.sh baby locations

--- SoftLepLooper/do.sh
+++ SoftLepLooper/do.sh
@@ -2,18 +2,15 @@
 
 make -j12
 
-# INDIR=/nfs-6/userdata/mt2/V00-01-09_25ns_diboson
-# OUTDIR=output/softLepSR/
-
-INDIR=/nfs-6/userdata/mt2/V00-01-07_25ns_miniaodv2_softLepSR
+INDIR=/nfs-6/userdata/mt2/V00-01-09_25ns_softLepSR/
 OUTDIR=output/softLepSR/
 
-# INDIR=/nfs-6/userdata/mt2/V00-01-07_25ns_miniaodv2_softLepCR
+# INDIR=/nfs-6/userdata/mt2/V00-01-09_25ns_softLepCR/
 # OUTDIR=output/softLepCR/
 
 #declare -a Samples=(data_Run2015C data_Run2015D ttsl_mg_lo ttdl_mg_lo wjets_ht zinv_ht singletop qcd_ht ttw ttz ttg tth dyjetsll_ht gjet_ht)
-declare -a Samples=(wjets_ht zinv_ht singletop ttdl ttsl ttg tth ttw ttz dyjetsll qcd_ht)
-#declare -a Samples=(ww)
+declare -a Samples=(wjets_ht zinv_ht singletop ttdl ttsl ttg tth ttw ttz dyjetsll qcd_ht ww)
+#declare -a Samples=(data_Run2015D)
 
 mkdir -p ${OUTDIR}
 mkdir -p logs
