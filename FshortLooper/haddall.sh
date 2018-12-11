87831891bca8521b05ceedb9e070f92076d665ca
dpg <dgilbert@physics.ucsd.edu>
2018-12-10 18:16:07 -0800

    short track convenience scripts

---
+++ FshortLooper/haddall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$#" -eq 0 ]; then
+    echo "Need a tag"
+    return
+fi
+
+tag=$1
+
+hadd -f output_merged/data_2017_${tag}.root output_unmerged/2017_${tag}/data/*.root
+hadd -f output_merged/mc_2017_${tag}.root output_unmerged/2017_${tag}/*.root
+hadd -f output_merged/data_2016_${tag}.root output_unmerged/2016_${tag}/data/*.root
+hadd -f output_merged/mc_2016_${tag}.root output_unmerged/2016_${tag}/*.root
--- FshortLooper/postprocess.sh
+++ FshortLooper/postprocess.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+if [ "$#" -eq 0 ]; then
+    echo "Need a tag"
+    return
+fi
+
 tag=$1
 
-declare -a inputs=(data_2017_${tag}.root mc_2017_${tag}.root data_2016_${tag}.root)
+declare -a inputs=(data_2017_${tag}.root mc_2017_${tag}.root data_2016_${tag}.root mc_2016_${tag}.root)
+#declare -a inputs=(data_2017_${tag}.root data_2016_${tag}.root)
 
 for input in ${inputs[@]}; do
-    python calculate_TFs.py output_merged/${input} &> /dev/null &
-    python ewk_dists.py output_merged/${input} &> /dev/null &
-    python ewk_contam.py output_merged/${input} &> /dev/null &
+    command="nohup python calculate_TFs.py output_merged/${input} &> /dev/null &"
+    eval $command
+    command="nohup python ewk_dists.py output_merged/${input} &> /dev/null &"
+    eval $command
+    command="nohup python ewk_contam.py output_merged/${input} &> /dev/null &"
+    eval $command
 done
