293d1fe9358d71be7e60d0b6f8c81516671feb5f
Bennett Marsh <bmarsh@physics.ucsb.edu>
2019-02-01 14:48:11 -0800

    adding corruption checker

---
+++ babymaker/batchsubmit/NEW_WORKFLOW/checkForCorrupt.py
@@ -0,0 +1,19 @@
+import subprocess
+import glob
+
+indirs = glob.glob("/hadoop/cms/store/user/jguiang/chronosim/")
+
+badfiles = []
+for indir in indirs:
+    # print "Checking dir",indir
+    p = subprocess.Popen("hdfs fsck {0}".format(indir.replace("/hadoop","")).split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    lines = out.split("\n")
+
+    for line in lines:
+        if "CORRUPT blockpool" in line:
+            file = line.split(":")[0]
+            badfiles.append("/hadoop"+file)
+            print "    BAD FILE:",badfiles[-1]
+            # subprocess.call("hdfs dfs -rm "+file, shell=True)
+
