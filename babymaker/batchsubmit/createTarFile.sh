b3c4291e012f4f628a64f162920fe90b4544d55c
dpg <dgilbert@physics.ucsd.edu>
2019-03-07 10:20:31 -0800

    Revert, actually an issue with cmssw in wrapper.sh

--- babymaker/batchsubmit/createTarFile.sh
+++ babymaker/batchsubmit/createTarFile.sh
@@ -3,7 +3,7 @@
 mkdir -p job_input
 rm -r job_input/*
 cd ..
-cp -r *.so ScanChain.* jetCorrections jsons btagsf lepsf data processBaby rebal *.pcm batchsubmit/job_input
+cp -r *.so ScanChain.* jetCorrections jsons btagsf lepsf data processBaby rebal batchsubmit/job_input
 cd batchsubmit/job_input
 tar -hcJf input.tar.xz *
 # gzip input.tar
