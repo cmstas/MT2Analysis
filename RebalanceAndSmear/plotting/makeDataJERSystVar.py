43305c4e35316581488742778ad9de73e9568dc9
Bennett Marsh <bmarsh@physics.ucsb.edu>
2019-03-25 15:44:16 -0700

    adding a couple of R&S plotting scripts

---
+++ RebalanceAndSmear/plotting/makeDataJERSystVar.py
@@ -0,0 +1,247 @@
+import numpy as np
+import ROOT
+import glob
+import os
+from common import *
+
+ROOT.gROOT.SetBatch(1)
+
+tag = "V00-10-09_ptBinned_XXX_JetID_PUID_BTagSFs_core2sigma"
+
+year = "All"
+lumi = 136.6
+
+# year = 2018
+# lumi = 58.8
+
+# year = 2017
+# lumi = 41.5
+
+# year = 2016
+# lumi = 35.9
+
+dir = "looper_output/V00-10-09_ptBinned_XXX_JetID_PUID_BTagSFs_core2sigma/data{0}/".format(year)
+dir_UP = "looper_output/V00-10-09_ptBinned_XXX_JetID_PUID_BTagSFs_core2sigma_jerUP/data{0}/".format(year)
+dir_DOWN = "looper_output/V00-10-09_ptBinned_XXX_JetID_PUID_BTagSFs_core2sigma_jerDOWN/data{0}/".format(year)
+
+NBINS = sum([len(x[1]) for x in topo_reg_defs.items()])
+
+hrs = ROOT.TH1D("hrs","",NBINS,0,NBINS)
+hUP = ROOT.TH1D("hUP","",NBINS,0,NBINS)
+hDOWN = ROOT.TH1D("hDOWN","",NBINS,0,NBINS)
+
+h_evts_rs = ROOT.TH1D("h_evts_rs","",1,0,2)
+h_evts_UP = ROOT.TH1D("h_evts_UP","",1,0,2)
+h_evts_DOWN = ROOT.TH1D("h_evts_DOWN","",1,0,2)
+
+frs = ROOT.TFile(os.path.join(dir,"merged_hists.root"))
+fUP = ROOT.TFile(os.path.join(dir_UP,"merged_hists.root"))
+fDOWN = ROOT.TFile(os.path.join(dir_DOWN,"merged_hists.root"))
+ibin = 0
+ht_regs = ["VL","L","M","H","UH"]
+for ht_reg in ht_regs:
+  sum_rs = 0
+  sum_UP = 0
+  sum_DOWN = 0
+  top_regs = topo_reg_defs[ht_reg]
+  for top_reg in top_regs:
+    ibin+=1
+
+    if h_evts_rs:
+      h_evts_rs.Reset()
+    if h_evts_UP:
+      h_evts_UP.Reset()
+    if h_evts_DOWN:
+      h_evts_DOWN.Reset()
+    try:
+      h_evts_rs = frs.Get("sr{0}{1}/h_Events_w".format(top_reg,ht_reg))
+    except:
+      pass
+    try:
+      h_evts_UP = fUP.Get("sr{0}{1}/h_Events_w".format(top_reg,ht_reg))
+    except:
+      pass
+    try:
+      h_evts_DOWN = fDOWN.Get("sr{0}{1}/h_Events_w".format(top_reg,ht_reg))
+    except:
+      pass
+
+    if h_evts_rs:
+      hrs.SetBinContent(ibin, h_evts_rs.GetBinContent(1))
+      hrs.SetBinError(ibin, h_evts_rs.GetBinError(1))
+    else:
+      hrs.SetBinContent(ibin, 0)
+      hrs.SetBinError(ibin, 0)
+
+    if h_evts_UP:
+      hUP.SetBinContent(ibin, h_evts_UP.GetBinContent(1))
+      hUP.SetBinError(ibin, h_evts_UP.GetBinError(1))
+    else:
+      hUP.SetBinContent(ibin, 0)
+      hUP.SetBinError(ibin, 0)
+    if h_evts_DOWN:
+      hDOWN.SetBinContent(ibin, h_evts_DOWN.GetBinContent(1))
+      hDOWN.SetBinError(ibin, h_evts_DOWN.GetBinError(1))
+    else:
+      hDOWN.SetBinContent(ibin, 0)
+      hDOWN.SetBinError(ibin, 0)
+
+    sum_rs += hrs.GetBinContent(ibin)
+    sum_UP += hUP.GetBinContent(ibin)
+    sum_DOWN += hDOWN.GetBinContent(ibin)
+      
+        
+ROOT.gStyle.SetOptStat(0)
+
+c = ROOT.TCanvas("c","c",900,600)
+
+pads = []
+pads.append(ROOT.TPad("1","1",0.0,0.18,1.0,1.0))
+pads.append(ROOT.TPad("2","2",0.0,0.0,1.0,0.19))
+
+pads[0].SetTopMargin(0.08)
+pads[0].SetBottomMargin(0.13)
+pads[0].SetRightMargin(0.05)
+pads[0].SetLeftMargin(0.10)
+
+pads[1].SetRightMargin(0.05)
+pads[1].SetLeftMargin(0.10)
+
+pads[0].Draw()
+pads[1].Draw()
+pads[0].cd()
+
+pads[0].SetLogy(1)
+pads[1].SetTickx(1)
+pads[0].SetTicky(1)
+pads[1].SetTicky(1)
+
+hrs.SetLineColor(ROOT.kBlack)
+hrs.SetMarkerColor(ROOT.kBlack)
+hrs.SetMarkerStyle(20)
+
+hUP.SetLineColor(ROOT.kGreen+2)
+hUP.SetMarkerColor(ROOT.kGreen+2)
+hUP.SetMarkerStyle(20)
+
+hDOWN.SetLineColor(ROOT.kRed)
+hDOWN.SetMarkerColor(ROOT.kRed)
+hDOWN.SetMarkerStyle(20)
+
+hrs.GetYaxis().SetRangeUser(1e-1,1e5)
+hrs.GetXaxis().SetLabelSize(0)
+
+hrs.Draw("PE")
+hUP.Draw("PE SAME")
+hDOWN.Draw("PE SAME")
+
+bin_width = (1-pads[0].GetLeftMargin()-pads[0].GetRightMargin()) / NBINS
+bin_divisions = [0]
+for i in range(len(ht_regs)):
+    bin_divisions.append(bin_divisions[i]+len(topo_reg_defs[ht_regs[i]]))
+bin_divisions = bin_divisions[1:-1]
+line = ROOT.TLine()
+line.SetLineStyle(2)
+for ix in bin_divisions:
+    x = pads[0].GetLeftMargin() + ix * bin_width
+    line.DrawLineNDC(x,1-pads[0].GetTopMargin(),x,pads[0].GetBottomMargin())
+
+
+leg = ROOT.TLegend(0.76,0.78,0.94,0.9)
+leg.AddEntry(hrs, "R&S, JER central val")
+leg.AddEntry(hUP, "R&S, JER UP")
+leg.AddEntry(hDOWN, "R&S, JER DOWN")
+leg.Draw()
+
+text = ROOT.TLatex()
+text.SetNDC(1)
+text.SetTextSize(0.03)
+text.SetTextAlign(22)
+modbindivs = [0] + bin_divisions + [hrs.GetNbinsX()]
+names = ["Very Low", "Low", "Medium", "High", "Extreme"]
+xposs = [pads[0].GetLeftMargin() + 0.5*(modbindivs[i]+modbindivs[i+1])*bin_width for i in range(len(modbindivs)-1)]
+for i in range(len(modbindivs)-1):
+  ypos = 0.79
+  if names[i]=="Extreme":
+    ypos = 0.72
+  text.SetTextColor(ROOT.kBlack)
+  text.DrawLatex(xposs[i],ypos,"{0} H_{{T}}".format(names[i]))
+  yrs, ers = 0.0, ROOT.Double(0.0)
+  yUP, eUP = 0.0, ROOT.Double(0.0)
+  yDOWN, eDOWN = 0.0, ROOT.Double(0.0)
+  yrs = hrs.IntegralAndError(modbindivs[i]+1, modbindivs[i+1], ers)
+  yUP = hUP.IntegralAndError(modbindivs[i]+1, modbindivs[i+1], eUP)
+  yDOWN = hDOWN.IntegralAndError(modbindivs[i]+1, modbindivs[i+1], eDOWN)
+  rUP = yUP/yrs
+  eUP = rUP * np.sqrt((eUP/yUP)**2 + (ers/yrs)**2)
+  rDOWN = yDOWN / yrs
+  eDOWN = rDOWN * np.sqrt((eDOWN/yDOWN)**2 + (ers/yrs)**2)
+  text.SetTextColor(ROOT.kGreen+2)
+  text.DrawLatex(xposs[i], ypos-0.03, "{0:.2f} #pm {1:.2f}".format(rUP,eUP))
+  text.SetTextColor(ROOT.kRed)
+  text.DrawLatex(xposs[i], ypos-0.06, "{0:.2f} #pm {1:.2f}".format(rDOWN,eDOWN))
+
+text.SetTextFont(42)
+text.SetTextSize(0.04)
+text.SetTextAlign(31)
+text.SetTextColor(ROOT.kBlack)
+text.DrawLatex(1.0-pads[0].GetRightMargin(), 1.0-pads[0].GetTopMargin()+0.01,"{0} fb^{{-1}} (13 TeV)".format(lumi))
+text.SetTextAlign(11)
+text.SetTextFont(62)
+text.DrawLatex(pads[0].GetLeftMargin()+0.01, 1.0-pads[0].GetTopMargin()+0.01,"CMS Preliminary")
+
+
+binLabels_all = []
+for ht_reg in ht_regs:
+    binLabels_all += [binLabels[tr] for tr in topo_reg_defs[ht_reg]]
+text = ROOT.TLatex()
+text.SetNDC(1)
+text.SetTextAlign(32)
+text.SetTextAngle(90)
+text.SetTextSize(min(bin_width * 1.5,0.027))
+text.SetTextFont(42)
+for ibin in range(NBINS):
+  x = pads[0].GetLeftMargin() + (ibin+0.5)*bin_width
+  y = pads[0].GetBottomMargin()-0.009
+  text.DrawLatex(x, y, binLabels_all[ibin])    
+
+
+## ratio
+pads[1].cd()
+
+h_ratioUP = hUP.Clone("h_ratio")
+h_ratioUP.Divide(hrs)
+h_ratioDOWN = hDOWN.Clone("h_ratio")
+h_ratioDOWN.Divide(hrs)
+
+for i in range(NBINS):
+  h_ratioUP.SetBinError(i, 0)
+  h_ratioDOWN.SetBinError(i, 0)
+
+h_ratioUP.GetYaxis().SetRangeUser(0.5,1.5)
+h_ratioUP.GetYaxis().SetNdivisions(505)
+h_ratioUP.GetYaxis().SetTitle("R&S/Obs")
+h_ratioUP.GetYaxis().SetTitleSize(0.16)
+h_ratioUP.GetYaxis().SetTitleOffset(0.18)
+h_ratioUP.GetYaxis().SetLabelSize(0.13)
+h_ratioUP.GetYaxis().CenterTitle()
+h_ratioUP.GetYaxis().SetTickLength(0.02)
+h_ratioUP.GetXaxis().SetLabelSize(0)
+h_ratioUP.GetXaxis().SetTitle("")
+h_ratioUP.GetXaxis().SetNdivisions(NBINS,0,0)
+h_ratioUP.GetXaxis().SetTickSize(0.06)
+h_ratioUP.SetMarkerStyle(20)
+h_ratioUP.SetMarkerSize(1.0)
+h_ratioUP.SetLineWidth(1)
+
+h_ratioUP.Draw("PE")
+h_ratioDOWN.Draw("PE SAME")
+
+line = ROOT.TLine()
+line.DrawLine(0,1,NBINS,1)
+
+username = os.environ["USER"]
+c.SaveAs("/home/users/{0}/public_html/mt2/RebalanceAndSmear/{1}/data{2}_JER_systVar.pdf".format(username, tag, year))
+c.SaveAs("/home/users/{0}/public_html/mt2/RebalanceAndSmear/{1}/data{2}_JER_systVar.png".format(username, tag, year))
+
+# raw_input()
---
+++ RebalanceAndSmear/plotting/makeSigmaSoftVarPlot.py
@@ -0,0 +1,239 @@
+import numpy as np
+import ROOT
+import glob
+import os
+
+ROOT.gROOT.SetBatch(1)
+
+tag = "V00-10-04_ptBinned_94x_JetID_PUID_BTagSFs_core2sigma"
+
+dir = "looper_output/V00-10-04_ptBinned_94x_JetID_PUID_BTagSFs_core2sigma/qcd"
+dir_sigma15 = "looper_output/V00-10-04_ptBinned_94x_JetID_PUID_BTagSFs_core2sigma_sigmasoft15/qcd"
+dir_sigma25 = "looper_output/V00-10-04_ptBinned_94x_JetID_PUID_BTagSFs_core2sigma_sigmasoft25/qcd"
+dir_noRS = "../SmearLooper/output/V00-10-04_94x_2017_noRS/"
+
+hrs = ROOT.TH1D("hrs","",51,0,51)
+h15 = ROOT.TH1D("h15","",51,0,51)
+h25 = ROOT.TH1D("h25","",51,0,51)
+hnrs = ROOT.TH1D("hnrs","",51,0,51)
+
+h_evts_rs = ROOT.TH1D("h_evts_rs","",1,0,2)
+h_evts_15 = ROOT.TH1D("h_evts_15","",1,0,2)
+h_evts_25 = ROOT.TH1D("h_evts_25","",1,0,2)
+h_evts_nrs = ROOT.TH1D("h_evts_nrs","",1,0,2)
+
+ibin = 0
+for ht_reg in ["VL","L","M","H","UH"]:
+    sum_rs = 0
+    sum_rs15 = 0
+    sum_rs25 = 0    
+    top_regs = range(1,12)
+    if ht_reg=="VL":
+        top_regs = [1,2,3,12,13,14,15]
+    for top_reg in top_regs:
+        ibin+=1
+        # print ibin
+
+        h_evts_rs.Reset()
+        h_evts_15.Reset()
+        h_evts_25.Reset()
+        h_evts_nrs.Reset()
+        bn = "merged_hists.root"
+        frs = ROOT.TFile(os.path.join(dir,bn))
+        f15 = ROOT.TFile(os.path.join(dir_sigma15,bn))
+        f25 = ROOT.TFile(os.path.join(dir_sigma25,bn))
+        fnrs = ROOT.TFile(os.path.join(dir_noRS,"qcd_ht.root"))
+        try:
+            h_evts_rs.Add(frs.Get("sr{0}{1}/h_Events_w".format(top_reg,ht_reg)))
+        except:
+            pass
+        try:
+            h_evts_15.Add(f15.Get("sr{0}{1}/h_Events_w".format(top_reg,ht_reg)))
+        except:
+            pass
+        try:
+            h_evts_25.Add(f25.Get("sr{0}{1}/h_Events_w".format(top_reg,ht_reg)))
+        except:
+            pass
+        try:
+            h_evts_nrs.Add(fnrs.Get("sr{0}{1}/h_Events_w".format(top_reg,ht_reg)))
+        except:
+            pass
+
+        hrs.SetBinContent(ibin, h_evts_rs.GetBinContent(1))
+        hrs.SetBinError(ibin, h_evts_rs.GetBinError(1))
+
+        h15.SetBinContent(ibin, h_evts_15.GetBinContent(1))
+        h15.SetBinError(ibin, h_evts_15.GetBinError(1))
+
+        h25.SetBinContent(ibin, h_evts_25.GetBinContent(1))
+        h25.SetBinError(ibin, h_evts_25.GetBinError(1))
+
+        hnrs.SetBinContent(ibin, h_evts_nrs.GetBinContent(1))
+        hnrs.SetBinError(ibin, h_evts_nrs.GetBinError(1))
+        
+        sum_rs += h_evts_rs.GetBinContent(1)
+        sum_rs15 += h_evts_15.GetBinContent(1)
+        sum_rs25 += h_evts_25.GetBinContent(1)
+        
+    print "{0} HT: rs = {1}, rs15 = {2}, ratio = {3}".format(ht_reg, sum_rs, sum_rs15, sum_rs15/sum_rs)
+    print "{0} HT: rs = {1}, rs25 = {2}, ratio = {3}".format(ht_reg, sum_rs, sum_rs25, sum_rs25/sum_rs)        
+
+ROOT.gStyle.SetOptStat(0)
+
+c = ROOT.TCanvas("c","c",900,600)
+
+pads = []
+pads.append(ROOT.TPad("1","1",0.0,0.18,1.0,1.0))
+pads.append(ROOT.TPad("2","2",0.0,0.0,1.0,0.19))
+
+pads[0].SetTopMargin(0.08)
+pads[0].SetBottomMargin(0.13)
+pads[0].SetRightMargin(0.05)
+pads[0].SetLeftMargin(0.10)
+
+pads[1].SetRightMargin(0.05)
+pads[1].SetLeftMargin(0.10)
+
+pads[0].Draw()
+pads[1].Draw()
+pads[0].cd()
+
+pads[0].SetLogy(1)
+pads[1].SetTickx(1)
+pads[0].SetTicky(1)
+pads[1].SetTicky(1)
+
+hrs.SetLineColor(ROOT.kBlack)
+hrs.SetMarkerColor(ROOT.kBlack)
+hrs.SetMarkerStyle(20)
+
+h15.SetLineColor(ROOT.kBlue)
+h15.SetMarkerColor(ROOT.kBlue)
+h15.SetMarkerStyle(20)
+
+h25.SetLineColor(ROOT.kRed)
+h25.SetMarkerColor(ROOT.kRed)
+h25.SetMarkerStyle(20)
+
+hnrs.SetLineColor(401)
+hnrs.SetMarkerColor(401)
+hnrs.SetMarkerStyle(20)
+
+hnrs.GetYaxis().SetRangeUser(1e-3,1e3)
+hnrs.GetXaxis().SetLabelSize(0)
+
+hnrs.Draw("PE")
+hrs.Draw("PE SAME")
+h15.Draw("PE SAME")
+h25.Draw("PE SAME")
+
+NBINS = hrs.GetNbinsX()
+bindivs = [7,18,29,40]
+binwidth = (1-pads[0].GetLeftMargin()-pads[0].GetRightMargin()) / NBINS
+
+line = ROOT.TLine()
+line.SetLineStyle(2)
+for ix in bindivs:
+  x = pads[0].GetLeftMargin() + ix/51.0 * (1-pads[0].GetLeftMargin()-pads[0].GetRightMargin())
+  line.DrawLineNDC(x,1-pads[0].GetTopMargin(),x,pads[0].GetBottomMargin())
+
+leg = ROOT.TLegend(0.73,0.75,0.94,0.9)
+leg.AddEntry(hnrs, "QCD MC")
+leg.AddEntry(hrs, "R&S (#sigma_{soft} = 20 GeV)")
+leg.AddEntry(h15, "R&S (#sigma_{soft} = 15 GeV)")
+leg.AddEntry(h25, "R&S (#sigma_{soft} = 25 GeV)")
+leg.Draw()
+
+text = ROOT.TLatex()
+text.SetNDC(1)
+text.SetTextSize(0.03)
+text.SetTextAlign(22)
+modbindivs = [0] + bindivs + [hrs.GetNbinsX()]
+names = ["Very Low", "Low", "Medium", "High", "Extreme"]
+xposs = [pads[0].GetLeftMargin() + 0.5*(modbindivs[i]+modbindivs[i+1])*binwidth for i in range(len(modbindivs)-1)]
+for i in range(len(modbindivs)-1):
+  ypos = 0.79
+  if names[i]=="Extreme":
+    ypos = 0.72
+  text.SetTextColor(ROOT.kBlack)
+  text.DrawLatex(xposs[i],ypos,"{0} H_{{T}}".format(names[i]))
+  yrs, ers = 0.0, ROOT.Double(0.0)
+  y15, e15 = 0.0, ROOT.Double(0.0)
+  y25, e25 = 0.0, ROOT.Double(0.0)
+  yrs = hrs.IntegralAndError(modbindivs[i]+1, modbindivs[i+1], ers)
+  y15 = h15.IntegralAndError(modbindivs[i]+1, modbindivs[i+1], e15)
+  y25 = h25.IntegralAndError(modbindivs[i]+1, modbindivs[i+1], e25)
+  r15 = y15 / yrs
+  r25 = y25 / yrs
+  e15 = r15 * np.sqrt((e15/y15)**2 + (ers/yrs)**2)
+  e25 = r25 * np.sqrt((e25/y25)**2 + (ers/yrs)**2)
+  text.SetTextColor(ROOT.kBlue)
+  text.DrawLatex(xposs[i], ypos-0.03, "{0:.2f} #pm {1:.2f}".format(r15,e15))
+  text.SetTextColor(ROOT.kRed)
+  text.DrawLatex(xposs[i], ypos-0.06, "{0:.2f} #pm {1:.2f}".format(r25,e25))
+
+text.SetTextFont(42)
+text.SetTextSize(0.04)
+text.SetTextAlign(31)
+text.SetTextColor(ROOT.kBlack)
+text.DrawLatex(1.0-pads[0].GetRightMargin(), 1.0-pads[0].GetTopMargin()+0.01,"1 fb^{-1} (13 TeV)")
+text.SetTextAlign(11)
+text.SetTextFont(62)
+text.DrawLatex(pads[0].GetLeftMargin()+0.01, 1.0-pads[0].GetTopMargin()+0.01,"CMS Preliminary")
+
+binLabels_vl = ["2-3j, 0b", "2-3j, 1b", "2-3j, 2b", "#geq4j, 0b", "#geq4j, 1b", "#geq4j, 2b", "#geq2j, #geq3b"] 
+binLabels = ["2-3j, 0b", "2-3j, 1b", "2-3j, 2b", "4-6j, 0b", "4-6j, 1b", "4-6j, 2b", "#geq7j, 0b", "#geq7j, 1b", "#geq7j, 2b", "2-6j, #geq3b", "#geq7j, #geq3b"]
+text = ROOT.TLatex()
+text.SetNDC(1)
+text.SetTextAlign(32)
+text.SetTextAngle(90)
+text.SetTextSize(min(binwidth * 1.3,0.027))
+text.SetTextFont(42)
+for ibin in range(11):
+  x = pads[0].GetLeftMargin() + (ibin+0.5)*binwidth
+  y = pads[0].GetBottomMargin()-0.009
+  if ibin < 7:
+    text.DrawLatex(x,y,binLabels_vl[ibin])
+  text.DrawLatex(x+7*binwidth,y,binLabels[ibin])    
+  text.DrawLatex(x+18*binwidth,y,binLabels[ibin])
+  text.DrawLatex(x+29*binwidth,y,binLabels[ibin])
+  text.DrawLatex(x+40*binwidth,y,binLabels[ibin])
+
+
+
+## ratio
+pads[1].cd()
+
+h_ratio15 = h15.Clone("h_ratio")
+h_ratio15.Divide(hrs)
+
+h_ratio25 = h25.Clone("h_ratio")
+h_ratio25.Divide(hrs)
+
+h_ratio15.GetYaxis().SetRangeUser(0,2)
+h_ratio15.GetYaxis().SetNdivisions(505)
+h_ratio15.GetYaxis().SetTitle("Shift/R&S")
+h_ratio15.GetYaxis().SetTitleSize(0.16)
+h_ratio15.GetYaxis().SetTitleOffset(0.18)
+h_ratio15.GetYaxis().SetLabelSize(0.13)
+h_ratio15.GetYaxis().CenterTitle()
+h_ratio15.GetYaxis().SetTickLength(0.02)
+h_ratio15.GetXaxis().SetLabelSize(0)
+h_ratio15.GetXaxis().SetTitle("")
+h_ratio15.GetXaxis().SetNdivisions(51,0,0)
+h_ratio15.GetXaxis().SetTickSize(0.06)
+h_ratio15.SetMarkerStyle(20)
+h_ratio15.SetMarkerSize(1.0)
+h_ratio15.SetLineWidth(1)
+
+h_ratio15.Draw("PE")
+h_ratio25.Draw("PE SAME")
+
+line = ROOT.TLine()
+line.DrawLine(0,1,51,1)
+
+c.SaveAs("/home/users/bemarsh/public_html/mt2/RebalanceAndSmear/{0}/mc_sigmasoft_var.pdf".format(tag))
+c.SaveAs("/home/users/bemarsh/public_html/mt2/RebalanceAndSmear/{0}/mc_sigmasoft_var.png".format(tag))
+
+
