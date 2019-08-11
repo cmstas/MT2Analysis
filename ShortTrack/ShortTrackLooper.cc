f0559677bf9bac634a7218fe4d1a7b6dcd83c00b
dpg <dgilbert@physics.ucsd.edu>
2019-08-11 11:21:44 -0700

    histograms inclusive in Nj incorrectly used fshort inclusive in pt (we don't use these hists, but fixing for the sake of principle)

--- ShortTrack/ShortTrackLooper.cc
+++ ShortTrack/ShortTrackLooper.cc
@@ -1852,7 +1852,7 @@ int ShortTrackLooper::loop (TChain* ch, char * outtag, std::string config_tag, c
     if (nSTCp4_lo > 0) {
       hist_lo->Fill(P4idx-0.5,2.0,weight*nSTCp4_lo); // Fill CR
       hist_Nj_lo->Fill(P4idx-0.5,2.0,weight*nSTCp4_lo); // Fill CR
-      hist_lo->Fill(P4idx-0.5,1.0, weight * ( fs_P4 * nSTCp4_lo) ); // Fill expected SR count
+      hist_lo->Fill(P4idx-0.5,1.0, weight * ( fs_P4_hi * nSTCp4_lo) ); // Fill expected SR count
       hist_Nj_lo->Fill(P4idx-0.5,1.0, weight * (t.nJet30 > 3 ? fs_Nj4_P4_lo : fs_Nj23_P4_lo) * nSTCp4_lo ); // Fill expected SR count with separate Nj fs
     }
     
@@ -1898,7 +1898,7 @@ int ShortTrackLooper::loop (TChain* ch, char * outtag, std::string config_tag, c
     if (nSTCp4_hi > 0) {
       hist_hi->Fill(P4idx-0.5,2.0,weight*nSTCp4_hi); // Fill CR
       hist_Nj_hi->Fill(P4idx-0.5,2.0,weight*nSTCp4_hi); // Fill CR
-      hist_hi->Fill(P4idx-0.5,1.0, weight * ( fs_P4 * nSTCp4_hi) ); // Fill expected SR count
+      hist_hi->Fill(P4idx-0.5,1.0, weight * ( fs_P4_hi * nSTCp4_hi) ); // Fill expected SR count
       hist_Nj_hi->Fill(P4idx-0.5,1.0, weight * (t.nJet30 > 3 ? fs_Nj4_P4_hi : fs_Nj23_P4_hi) * nSTCp4_hi ); // Fill expected SR count with separate Nj fs
     }
     
